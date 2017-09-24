cc5f5ebac464799221dc9c4c59f0599d3425f3b6
Gautam Naik <gautamnaik1994@gmail.com>
2017-09-24 21:20:53 +0530

    added google oauth

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-const express = require('express');
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const keys = require("./config/keys");
+
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ hi: 'HHHHAAAA' });
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: "/auth/google/callback"
+    },
+    (accessToken,refreshToken,profile,done) => {
+        console.log("AcessToken", accessToken);
+        console.log("RefreshToken", refreshToken);  
+        console.log("Profile", profile);  
+    }
+  )
+);
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["profile", "email"]
+  })
+);
+app.get("/auth/google/callback", passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
-    console.log('Server listening on port ', PORT );
+  console.log("Server listening on port ", PORT);
 });
